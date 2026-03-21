7f9861ab22b0f01dcaaa5b28c9365e246680d8a6
Saersa <dorandoran911@gmail.com>
2026-03-21 16:40:28 +0100

    Create PremiumTest.lua

---
+++ Games/Game 1/PremiumTest.lua
@@ -0,0 +1,53 @@
+local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Saersa/Vyper-Development/refs/heads/main/WindUI_Custom.lua"))()
+local Window = WindUI:CreateWindow({
+    Title = "My Super Hub",
+    Icon = "door-open", -- lucide icon
+    Author = "by .ftgs and .ftgs",
+    Folder = "MySuperHub",
+    
+
+    NewElements = true,
+
+    -- ↓ This all is Optional. You can remove it.
+    Size = UDim2.fromOffset(580, 460),
+    MinSize = Vector2.new(580, 460),
+    MaxSize = Vector2.new(580, 460),
+    Transparent = true,
+    Theme = "Vyper",
+    SideBarWidth = 200,
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = false,
+    ScrollBarEnabled = false,
+    User = {
+        Enabled = true,
+        Anonymous = false,
+        Callback = function()
+            print("clicked")
+        end,
+    },
+     KeySystem = {                                                   
+        Note = "By completing this key system you support the devs.",        
+        API = {                                                     
+            {
+                Type = "pandadevelopment",
+                ServiceId = "myServiceId",
+            },                                                      
+        },                                                          
+    }, 
+    Topbar = {
+        Height = 44,
+        ButtonsType = "Default",
+    },
+})
+
+Window:Tag({
+    Title = Window:IsPremium() and "Premium User" or "Free User",
+    Icon = "github",
+    Color = Color3.fromHex("#30ff6a"),
+    Radius = 8,
+})
+
+if Window:IsPremium() then
+    local PremiumTab = Window:Tab({ Title = "Premium ⭐" })
+    -- add premium elements...
+end
